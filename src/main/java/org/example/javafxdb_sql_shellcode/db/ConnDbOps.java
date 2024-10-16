ab8360204dbc419c326f16254790f6acfdf2fbf8
ZuXinChne <chenz@farmingdale.edu>
2024-10-16 12:24:28 -0400

    mysql database info and able to connect to my database

--- src/main/java/org/example/javafxdb_sql_shellcode/db/ConnDbOps.java
+++ src/main/java/org/example/javafxdb_sql_shellcode/db/ConnDbOps.java
@@ -16,10 +16,10 @@ import java.sql.Statement;
  * @author MoaathAlrajab
  */
 public class ConnDbOps {
-    final String MYSQL_SERVER_URL = "jdbc:mysql://localhost/";
-    final String DB_URL = "jdbc:mysql://localhost/DBname";
-    final String USERNAME = "admin";
-    final String PASSWORD = "password";
+    final String MYSQL_SERVER_URL = "jdbc:mysql://csc311chenserver.mysql.database.azure.com/";
+    final String DB_URL = "jdbc:mysql://csc311chenserver.mysql.database.azure.com/DBname";
+    final String USERNAME = "csc311chenserver";
+    final String PASSWORD = "Chen1500Server";
     
     public  boolean connectToDatabase() {
         boolean hasRegistredUsers = false;
